3361e6ad5964d1cce1012e66d84dbcb7c935f1d2
Script Raccoon <scriptraccoon@gmail.com>
2022-06-28 20:29:23 +0200

    add most styles

--- Vue/src/components/List.vue
+++ Vue/src/components/List.vue
@@ -43,7 +43,11 @@
     <ul>
         <template v-for="item in list" :key="item.id">
             <li :class="{ done: item.done }">
-                <button aria-label="Buy item" @click="item.done = !item.done">
+                <button
+                    class="actionButton buyButton"
+                    aria-label="Buy item"
+                    @click="item.done = !item.done"
+                >
                     <i
                         aria-hidden="true"
                         class="fa-solid fa-basket-shopping"
@@ -56,7 +60,11 @@
                     aria-label="Item name"
                     :disabled="item.done"
                 />
-                <button aria-label="Delete item" @click="deleteItem(item.id)">
+                <button
+                    class="actionButton deleteBtn"
+                    aria-label="Delete item"
+                    @click="deleteItem(item.id)"
+                >
                     <i aria-hidden="true" class="fa-solid fa-xmark"></i>
                 </button>
             </li>
@@ -67,5 +75,62 @@
 <style scoped>
     .done {
         opacity: 0.3;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+    menu {
+        margin: 20px 0px;
+        display: flex;
+        gap: 20px;
+        justify-content: center;
+    }
+    menu button {
+        background-color: var(--secondary-color);
+        color: white;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+
+    menu button:hover,
+    menu button:focus-visible {
+        background-color: var(--primary-color);
+    }
+
+    /* item styles */
+
+    .buyButton {
+        width: 30px;
+    }
+    li {
+        font-size: 20px;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        margin: 5px 0px;
+    }
+    input[type="text"] {
+        flex: 1;
+        padding: 10px;
+        border-radius: 6px;
+    }
+    input[type="text"]:focus {
+        background-color: #eee;
+    }
+    li.done {
+        opacity: 0.3;
+    }
+    .deleteBtn:where(:not(:focus-visible, :hover)) {
+        color: red;
+    }
+    .actionButton {
+        color: var(--secondary-color);
+    }
+
+    .actionButton:hover,
+    .actionButton:focus-visible {
+        color: var(--primary-color);
     }
 </style>
