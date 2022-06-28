40dbe251c6a76436a67ca12523bf2253ad8aeb0e
Script Raccoon <scriptraccoon@gmail.com>
2022-06-28 13:08:48 +0200

    initialize Vue project

---
+++ Vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import "./global.css";
+    import Header from "./components/Header.vue";
+</script>
+
+<template>
+    <Header></Header>
+    <main>List goes here</main>
+</template>
+
+<style>
+    main {
+        max-width: 720px;
+        margin: 0 auto;
+        padding: 15px;
+    }
+</style>
---
+++ Vue/src/components/Header.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <header>
+        <h1>Shopping List</h1>
+    </header>
+</template>
+
+
+<style scoped>
+    header {
+        background-color: var(--primary-color);
+        color: white;
+        padding: 25px 0px;
+    }
+
+    h1 {
+        text-align: center;
+        text-shadow: 1px 1px 5px #0008;
+    }
+</style>
