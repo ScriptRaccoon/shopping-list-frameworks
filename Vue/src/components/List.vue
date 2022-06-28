648d28c82bad01e0f10bf10c7e89c231c192bf35
Script Raccoon <scriptraccoon@gmail.com>
2022-06-28 20:24:27 +0200

    add and delete item

--- Vue/src/components/List.vue
+++ Vue/src/components/List.vue
@@ -9,9 +9,37 @@
     function deleteItem(id) {
         list.value = list.value.filter((item) => item.id != id);
     }
+
+    function addItem() {
+        const item = {
+            name: "",
+            done: false,
+            id: crypto.randomUUID(),
+            new: true,
+        };
+        list.value.unshift(item);
+    }
+
+    function deleteList() {
+        if (window.confirm("Do you really want to delete the whole list?")) {
+            list.value = [];
+        }
+    }
 </script>
 
 <template>
+    <menu>
+        <button @click="addItem" aria-label="Add item">
+            <i aria-hidden="true" class="fa-solid fa-plus"></i>
+        </button>
+        <button
+            v-if="list.length > 0"
+            @click="deleteList"
+            aria-label="Delete list"
+        >
+            <i aria-hidden="true" class="fa-solid fa-trash-can"></i>
+        </button>
+    </menu>
     <ul>
         <template v-for="item in list" :key="item.id">
             <li :class="{ done: item.done }">
