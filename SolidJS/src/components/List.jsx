26e7bef9054d7630725396bc26e01d510fc13302
Script Raccoon <scriptraccoon@gmail.com>
2022-06-30 16:37:38 +0200

    basic loop

--- SolidJS/src/components/List.jsx
+++ SolidJS/src/components/List.jsx
@@ -1,9 +1,32 @@
 import "./List.module.css";
+import { For } from "solid-js";
 
 function List() {
+    const list = [
+        {
+            id: 1,
+            name: "A",
+            done: false,
+        },
+        {
+            id: 2,
+            name: "B",
+            done: true,
+        },
+        {
+            id: 3,
+            name: "C",
+            done: false,
+        },
+    ];
     return (
-        <p>I am the list</p>
-    )
+        <For
+            each={list}
+            fallback={<p style="text-align: center;">Add items</p>}
+        >
+            {(item) => <div>{item.name}</div>}
+        </For>
+    );
 }
 
 export default List;
