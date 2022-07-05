5aca1f387e8035904b5fa3d7724a6d63859a743b
Script Raccoon <scriptraccoon@gmail.com>
2022-07-05 12:38:51 +0200

    add functionality

--- Alpine/src/main.js
+++ Alpine/src/main.js
@@ -1,3 +1,13 @@
 import Alpine from "alpinejs";
 window.Alpine = Alpine;
+
+window.newItem = () => {
+    return {
+        id: crypto.randomUUID(),
+        name: "",
+        done: false,
+        new: true,
+    };
+};
+
 Alpine.start();
